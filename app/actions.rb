645cc4ee9755447508b0083cfd9cde04cc874e71
prateeksan <sanyal.prateek@gmail.com>
2016-01-27 23:32:37 +0000

    All done. User can sign up, sign in, sign out

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+end
+
 # Homepage (Root path)
 get '/' do
   erb :index
@@ -9,7 +15,31 @@ get '/user/signup' do
 end
 
 get '/user/signin' do
+  @error = nil
   erb :'/session/new'
+end
+
+get '/user/signout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+post '/user/signin' do 
+  @user = User.find_by(username: params[:username])
+
+  if @user
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      #currently set to redirect to landing page. In future we can redirect to wherever the functionality is 
+      redirect '/'
+    else
+      @error = "Incorrect pasword, try again"
+      erb :'/session/new'
+    end      
+  else
+    @error = "User not found, try again"
+    erb :'/session/new'
+  end
 end
 
 post '/user/signup' do
@@ -27,6 +57,7 @@ post '/user/signup' do
     medical_history: params[:medical_history]
     )
   if @user.save
+    #currently set to redirect to landing page. In future we can redirect to wherever the functionality is 
     redirect '/'
   else
     erb :'/users/new'
