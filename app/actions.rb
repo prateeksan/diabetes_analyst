669668986ca084083e317f4582387da0cdd2945a
prateeksan <sanyal.prateek@gmail.com>
2016-01-27 21:23:09 +0000

    Sign up complete with password authentication and vallidations

--- app/actions.rb
+++ app/actions.rb
@@ -3,10 +3,32 @@ get '/' do
   erb :index
 end
 
-get '/signup' do
+get '/user/signup' do
+  @user = User.new
   erb :'/users/new'
 end
 
-get '/signin' do
+get '/user/signin' do
   erb :'/session/new'
+end
+
+post '/user/signup' do
+  @user = User.new(
+    username: params[:username],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation],
+    first_name: params[:first_name],
+    middle_name: params[:middle_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    birth_date: params[:birth_date],
+    diagnosis_date: params[:diagnosis_date],
+    height: params[:height],
+    medical_history: params[:medical_history]
+    )
+  if @user.save
+    redirect '/'
+  else
+    erb :'/users/new'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321144528) do
+ActiveRecord::Schema.define(version: 20160127184120) do
 
-  create_table "users", force: true do |t|
-    t.string "name"
-    t.string "email"
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "middle_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.date     "birth_date"
+    t.date     "diagnosis_date"
+    t.integer  "height"
+    t.text     "medical_history"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
