4ac11780fb0b2529b0fd818b3a24967c269f4dd5
FantasticJimmy <dboyjimmy@gmail.com>
2016-02-04 23:49:40 +0000

    envirment.rb pry commented out

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ require 'sinatra/contrib/all' # Requires cookies, among other things
 require 'sinatra/partial'
 require "open-uri"
 require 'prawn'
-require 'pry' if development?
+# require 'pry' if development?
 
 require 'bcrypt'
 
